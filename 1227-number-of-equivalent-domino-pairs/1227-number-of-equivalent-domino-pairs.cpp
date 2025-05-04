3fb4eb1d09dcae86c22e1b2a9a401dcef450828a
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-05-04 16:46:03 +0600

    Time: 0 ms (100%), Space: 26.2 MB (48.5%) - LeetHub

---
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        
+        map<pair<int,int>, int> mp;
+        for (auto& d : dominoes) {
+            
+            int a = min(d[0], d[1]);
+            int b = max(d[0], d[1]);
+            mp[{a, b}]++;
+        }
+
+        int count = 0;
+        for(auto it : mp) {
+            int a = it.second;
+            count += (a * (a-1)) / 2;
+        }
+        return count;
+    }
+};
