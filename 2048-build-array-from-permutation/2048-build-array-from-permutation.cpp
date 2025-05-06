7f1495d1473b37ed885cab1d6dcf665cd321da05
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-05-06 12:34:44 +0600

    Time: 0 ms (100%), Space: 21 MB (18.1%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-
         vector<int> vec;
         for (int i = 0; i < nums.size(); i++) {
             vec.push_back(nums[nums[i]]);
