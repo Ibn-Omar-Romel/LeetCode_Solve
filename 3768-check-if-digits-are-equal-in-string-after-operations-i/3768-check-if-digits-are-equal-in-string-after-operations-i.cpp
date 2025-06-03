d17c0d3eaf3762bb78042460e625dbea192f5531
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-06-03 22:16:01 +0600

    Time: 7 ms (65.34%), Space: 9.9 MB (74.14%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -6,17 +6,13 @@ public:
 
             string st = "";
             for (int i = 0; i < s.size()-1; i++) {
-                int a = s[i] -'0';
-                int b = s[i+1] -'0';
-
-                int c = (a+b) % 10;
-
-                st.push_back((char)c);
+                int a = ((s[i]-'0') + (s[i+1]-'0')) % 10;
+                char ch = a + '0';
+                st.push_back(ch);
             }
             s = st;
         }
-        if(s[0] == s[1])
-            return true;
-        return false;
+        
+        return s[0] == s[1];
     }
 };
