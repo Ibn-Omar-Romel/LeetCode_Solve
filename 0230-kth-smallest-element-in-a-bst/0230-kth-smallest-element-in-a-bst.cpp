4c8e8ce1ddd7037dd34dcc85b37e749337111799
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-06-05 22:55:54 +0600

    Time: 3 ms (9.64%), Space: 24.6 MB (17.7%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void inOrder(TreeNode* root, vector<int>& vec) {
+
+        if(root == NULL)
+            return;
+        inOrder(root->left, vec);
+        vec.push_back(root->val);
+        inOrder(root->right, vec);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int>vec;
+        inOrder(root, vec);
+        sort(vec.begin(), vec.end());
+        return vec[k-1];
+    }
+};
