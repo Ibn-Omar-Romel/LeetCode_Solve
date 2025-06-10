856a41a9d5e5f52cd7ea4691ecef59a25aef0c58
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-06-11 00:47:15 +0600

    Time: 0 ms (100%), Space: 8.7 MB (75.25%) - LeetHub

--- 0961-long-pressed-name/0961-long-pressed-name.cpp
+++ 0961-long-pressed-name/0961-long-pressed-name.cpp
@@ -4,17 +4,13 @@ public:
         
         int n = name.size(), m = typed.size();
         int i = 0, j = 0;
-
         while(j < m) {
-
             if (name[i] == typed[j]) {
                 i++;
                 j++;
-            }
-            else if (j > 0 and typed[j] == typed[j-1]) {
+            } else if (j > 0 and typed[j] == typed[j-1]) {
                 j++;
-            }
-            else {
+            } else {
                 return false;
             }
         }
