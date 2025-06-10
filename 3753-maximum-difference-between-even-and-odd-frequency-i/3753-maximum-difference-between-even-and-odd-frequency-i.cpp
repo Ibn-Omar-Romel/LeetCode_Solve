3e0467f689ba63fde8142ba6d99488be0210ebaf
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-06-10 19:17:46 +0600

    Time: 0 ms (100%), Space: 9.3 MB (68.21%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int maxDifference(string s) {
-
         int n = s.size();
-        
-        vector<int>vec(26, 0);
+        vector<int> vec(26, 0);
         for (auto x : s) {
-            vec[x-'a']++;
+            vec[x - 'a']++;
         }
-
-        int maxOdd = INT_MIN;
-        int minEven = INT_MAX;
-
+        int maxOdd = INT_MIN, minEven = INT_MAX;
         for (int i = 0; i < 26; i++) {
-            if(vec[i] != 0) {
-                if(vec[i] % 2 == 0){
-                    minEven = min(minEven, vec[i]);
-                }
-                else {
-                    maxOdd = max(maxOdd, vec[i]);
-                }
+            if (vec[i] % 2) {
+                maxOdd = max(maxOdd, vec[i]);
+            } else if (vec[i]) {
+                minEven = min(minEven, vec[i]);
             }
         }
         return maxOdd - minEven;
-        
     }
 };
