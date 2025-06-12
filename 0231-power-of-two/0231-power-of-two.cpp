820382a35e2d200f467644119f91f828e15eb993
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-06-13 02:40:21 +0600

    Time: 0 ms (100%), Space: 7.8 MB (53.52%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+
+    bool isPowerOfTwo(int n) {
+
+        if ( n <= 0 )
+            return false;
+
+        // double b = log2(n);
+        // return b == (int)b ;
+        return (n && !(n & (n-1)));
+    }
+};
