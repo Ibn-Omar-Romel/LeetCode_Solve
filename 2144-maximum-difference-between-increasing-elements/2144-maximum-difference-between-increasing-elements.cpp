6c0db8e530b19f64f4aa7877ff71ce08d08545dd
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-06-16 14:33:11 +0600

    Time: 0 ms (100%), Space: 12.7 MB (5.67%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -3,14 +3,24 @@ public:
     int maximumDifference(vector<int>& nums) {
 
         int n = nums.size();
+        vector<int>pre(n), suff(n);
+        pre[0] = nums[0];
+        suff[n-1] = nums[n-1];
+
+        for (int i = 1; i < n; i++) {
+            pre[i] = min(pre[i-1], nums[i]);
+        }
+        for (int i = n-2; i >= 0; i--) {
+            suff[i] = max(suff[i+1], nums[i]);
+        }
+
         int ans = -1;
         for (int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                if (nums[i] < nums[j]) {
-                    ans = max(ans, nums[j]-nums[i]);
-                }
-            }
+            ans = max(suff[i] - pre[i], ans);
         }
+
+        if (ans == 0)
+            return -1;
         return ans;
     }
 };
