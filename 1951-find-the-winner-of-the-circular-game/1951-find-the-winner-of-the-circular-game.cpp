cf6ff158e3d4fbb6d24a3b416b54aed694a8f743
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-06-16 21:26:42 +0600

    Time: 21 ms (24.12%), Space: 26.5 MB (13.78%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int>vec(n);
+
+        queue<int>que;
         for (int i = 1; i <= n; i++) {
-            vec[i-1] = i;
+            que.push(i);
         }
 
-        int i = 0;
-        while(vec.size() != 1) {
-            int position = (i+k-1) % vec.size();
-            cout << position << " ";
-            vec.erase(vec.begin()+position);
-
-            i = position;
-        }   
-        return vec[0];
+        while(que.size() > 1) {
+            for (int i = 1; i < k; i++) {
+                int value = que.front();
+                que.pop();
+                que.push(value);
+            }
+            que.pop();
+        }
+        return que.front();
     }
 };
