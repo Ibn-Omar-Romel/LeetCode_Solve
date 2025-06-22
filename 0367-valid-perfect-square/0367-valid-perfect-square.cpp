8b42e2775102113c90534a94dcd15a2da387b6f3
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-06-23 00:17:31 +0600

    Time: 0 ms (100%), Space: 7.7 MB (87.27%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+
+        if(num == 1)
+            return true;
+        
+        int l = 1, r = num / 2;
+
+        while(l <= r) {
+
+            long long mid = l + (r-l) / 2;
+            long long value = mid * mid;
+
+            if (value == num){
+                return true;
+            }
+            else if (value < num) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+};
