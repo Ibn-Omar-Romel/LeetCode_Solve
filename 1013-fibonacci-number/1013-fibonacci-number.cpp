8c4394a7bd28501f86cbc7072f5da55c248ee8f4
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-07-09 13:33:10 +0600

    Time: 3 ms (66.98%), Space: 8.4 MB (11.05%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     int fib(int n) {
 
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
+        if (n <= 1)
+            return n;
 
-        return fib(n-1) + fib(n-2);
+        vector<int>vec(n+1,0);
+        vec[0] = 0;
+        vec[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            vec[i] = vec[i-1] + vec[i-2];
+        }
+
+        return vec[n];
     }
 };
