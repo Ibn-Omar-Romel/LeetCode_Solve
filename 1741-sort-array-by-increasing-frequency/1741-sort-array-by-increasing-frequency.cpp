0b43a71c7ac97fd741dee00205e127f656c88c72
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-07-10 23:11:13 +0600

    Time: 3 ms (54.51%), Space: 15 MB (81.03%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -2,27 +2,17 @@ class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
         
-        map<int,int>mp;
+        unordered_map<int, int>mp;
         for (auto x : nums) {
             mp[x]++;
         }
-        map<int, vector<int>>mpp;
-        for (auto it : mp) {
-            mpp[it.second].push_back(it.first);
-        }
 
-        vector<int>result;
-        for (auto it : mpp) {
-            vector<int>temp = it.second;
-            reverse(temp.begin(), temp.end());
-
-            for (int i = 0; i < temp.size(); i++) {
-                
-                for (int j = 0; j < it.first; j++) {
-                    result.push_back(temp[i]);
-                }
+        sort(nums.begin(), nums.end(), [&](int a, int b){
+            if (mp[a] != mp[b]) {
+                return mp[a] < mp[b];
             }
-        }
-        return result;
+            return a > b;
+        });
+        return nums;
     }
 };
