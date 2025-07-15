3932c6b611b0465fde482d19ae5469f62d22ceea
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-07-15 10:26:03 +0600

    Time: 0 ms (100%), Space: 8 MB (33.01%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string word) {
+
+        if (word.size() < 3)
+            return false;
+
+        bool vowel = false, consotant = false;
+        for (auto x : word) {
+
+            
+            if (isdigit(x)) {
+                continue;
+            }
+
+            x = tolower(x);
+            if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
+                vowel = true;
+            }
+            else if (isalpha(x)) {
+                consotant = true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        if (vowel and consotant)    
+            return true;
+        return false;
+    }
+};
