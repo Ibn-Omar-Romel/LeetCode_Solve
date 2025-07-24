22ba59504b57cf0ca32919eed0d625c4e5450bc2
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-07-25 00:22:23 +0600

    Time: 29 ms (55.52%), Space: 80.2 MB (58.62%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -3,21 +3,14 @@ public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
 
         sort(players.begin(), players.end());
-        sort(trainers.begin(), trainers.end());
-
-        
+        sort(trainers.begin(), trainers.end()); 
         int i = 0, j = 0;
-        int match = 0;
-
         while (i < players.size() and j < trainers.size()) {
             if (players[i] <= trainers[j] ) {
-                match++;
-                i++, j++;
+                i++;
             }
-            else {
-                j++;
-            }
+            j++;
         }  
-        return match;  
+        return i;  
     }
 };
