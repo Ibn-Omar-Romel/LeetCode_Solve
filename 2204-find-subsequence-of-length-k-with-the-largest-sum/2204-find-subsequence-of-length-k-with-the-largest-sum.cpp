2def035ed727b16b8132dfbf34953129314b9ded
Ibn Omar Romel <116564774+Ibn-Omar-Romel@users.noreply.github.com>
2025-07-26 21:08:36 +0600

    Time: 0 ms (100%), Space: 13.9 MB (49.35%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,25 +1,59 @@
 class Solution {
 public:
+    // vector<int> maxSubsequence(vector<int>& nums, int k) {
+
+    //     vector<int>temp = nums;
+    //     sort(temp.begin(),temp.end(), greater<int>());
+        
+    //     unordered_map<int, int>mp;
+    //     for (int i = 0; i < k; i++) {
+    //         mp[temp[i]]++;
+    //     }
+
+    //     vector<int>vec;
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         if (mp.find(nums[i]) != mp.end()) {
+    //             vec.push_back(nums[i]);
+    //             mp[nums[i]]--;
+    //             if (mp[nums[i]] == 0)
+    //                 mp.erase(nums[i]);
+    //         }
+    //     }
+    //     return vec;
+            
+    // }
+
+
     vector<int> maxSubsequence(vector<int>& nums, int k) {
 
-        vector<int>temp = nums;
-        sort(temp.begin(),temp.end(), greater<int>());
-        
-        unordered_map<int, int>mp;
-        for (int i = 0; i < k; i++) {
-            mp[temp[i]]++;
-        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-        vector<int>vec;
         for (int i = 0; i < nums.size(); i++) {
-            if (mp.find(nums[i]) != mp.end()) {
-                vec.push_back(nums[i]);
-                mp[nums[i]]--;
-                if (mp[nums[i]] == 0)
-                    mp.erase(nums[i]);
+            if (pq.size() < k) {
+                pq.push({nums[i], i});
+            }
+            else if (nums[i] > pq.top().first) {
+
+                pq.pop();
+                pq.push({nums[i], i});
             }
         }
-        return vec;
-            
+
+        vector<pair<int, int>>vec;
+        while(!pq.empty()) {
+            vec.push_back(pq.top());
+            pq.pop();
+        }
+
+        sort(vec.begin(), vec.end(), [](auto &a, auto &b) {
+            return a.second < b.second;
+        });
+
+        vector<int>result;
+        for (int i = 0; i < vec.size(); i++) {
+            result.push_back(vec[i].first);
+        }
+        return result;
+
     }
 };
